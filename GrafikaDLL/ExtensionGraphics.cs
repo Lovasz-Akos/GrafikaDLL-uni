75d6f7b5d4ea7c09f9fdb3371148cfe34a157555
Lovász Ákos <akos4x44@gmail.com>
2021-09-25 19:52:50 +0200

    took a stab at circle drawing...

--- GrafikaDLL/ExtensionGraphics.cs
+++ GrafikaDLL/ExtensionGraphics.cs
@@ -53,7 +53,7 @@ namespace GrafikaDLL
             float incY = dy / length;
             float incR = (c2.R - c1.R) / length;
             float incG = (c2.G - c1.G) / length;
-            float incB = (c2.B - c1.B) / length;  
+            float incB = (c2.B - c1.B) / length;
             float x = x1, y = y1;
             float R = c1.R, G = c1.G, B = c1.B;
             g.DrawRectangle(new Pen(Color.FromArgb((int)R, (int)G, (int)B)), x, y, 0.5f, 0.5f);
@@ -75,7 +75,39 @@ namespace GrafikaDLL
         public static void DrawLineMidPoint(this Graphics g,
             Pen pen, float x1, float y1, float x2, float y2)
         {
-            throw new NotImplementedException();
+            /*
+             VÁLTOZÓK        
+        EGÉSZ: D, DY, DX, X, Y, I;
+    ALGORITMUS
+        DX <- X1 - X0;
+        DY <- Y1 - Y0;
+        D <- 2 * DY - DX;
+        X <- X1;
+        Y <- Y1;
+        CIKLUS I <- 1..DX
+            PIXEL(X, Y, S);
+            HA (D > 0) AKKOR
+                Y <- Y + 1;
+                D <- D + 2 * (DY - DX);
+            KÜLÖNBEN
+                D <- D + 2 * DY;
+            HA_VÉGE;
+            X <- X + 1;
+        CIKLUS_VÉGE;
+ELJÁRÁS_VÉGE;
+
+             */
+
+            float d, dy, dx, x, y;
+            dx = x2 - x1;
+            dy = y2 - y1;
+            d = 2 * dy - dx;
+            x = x2;
+            y = y2;
+            for (int i = 0; i < dx; i++)
+            {
+                g.DrawRectangle(Pens.Black, x, y, 3, 3);
+            }
         }
         public static void DrawLineMidPoint(this Graphics g,
             Pen pen, PointF p1, PointF p2)
@@ -121,7 +153,97 @@ namespace GrafikaDLL
         public static void DrawCircle(this Graphics g, Pen pen, PointF C, float r)
         {
 
+            /* ELJÁRÁS MP_KOR_V1(EGÉSZ R, SZIN S);
+             VÁLTOZÓK
+
+             EGÉSZ: X, Y;
+             VALÓS: D;
+             ALGORITMUS
+
+             X <- 0;
+             Y <- R;
+             D <- 5 / 4 - R;
+             KORPONT(X, Y, S);
+             CIKLUS_AMÍG(Y > X)
+
+             HA(D < 0) AKKOR
+
+                 D <- D + 2 * X + 3;
+             KÜLÖNBEN
+
+                 D < -D + 2 * (X - Y) + 5;
+             Y <- Y - 1;
+             HA_VÉGE;
+             X <- X + 1;
+             KORPONT(X, Y, S);
+             CIKLUS_VÉGE;
+             ELJÁRÁS_VÉGE;
+            */
+
+            float x, y, d;
+            x = 0;
+            y = r;
+            d = 5 / 4 - r;
+            KorpontTukrozes(g, Pens.Black, x, y);
+            while (y > x)
+            {
+                if (d < 0)
+                {
+                    d = d + 2 * x + 3;
+                }
+                else
+                {
+                    d = d + 2 * (x - y) + 5;
+                    y--;
+                }
+                x++;
+                KorpontTukrozes(g, Pens.Black, x, y);
+            }
         }
+
+        /* ELJÁRÁS KORPONT(SZIN S, EGÉSZ X, EGÉSZ Y);
+         ALGORITMUS
+         PIXEL(X, Y, S);
+         PIXEL(X, -Y, S);
+         PIXEL(-X, Y, S);
+         PIXEL(-X, -Y, S);
+         PIXEL(X, Y, S);
+         PIXEL(X, -Y, S);
+         PIXEL(-X, Y, S);
+         PIXEL(-X, -Y, S);
+         ELJÁRÁS_VÉGE;
+        */
+
+        private static void KorpontTukrozes(this Graphics g, Pen S, float x, float y)
+        {
+            g.DrawRectangle(S, x, y, 3, 3);
+
+            g.DrawRectangle(S, x, -y, 3, 3);
+
+            g.DrawRectangle(S, -x, y, 3, 3);
+
+            g.DrawRectangle(S, -x, -y, 3, 3);
+
+            g.DrawRectangle(S, x, y, 3, 3);
+
+            g.DrawRectangle(S, x, -y, 3, 3);
+
+            g.DrawRectangle(S, -x, y, 3, 3);
+
+            g.DrawRectangle(S, -x, -y, 3, 3);
+
+
+            /*
+            DrawLineMidPoint(g, S, x, y, x, -y);
+
+            DrawLineMidPoint(g, S, -x, y, -x, -y);
+
+            DrawLineMidPoint(g, S, x, y, x, -y);
+
+            DrawLineMidPoint(g, S, -x, y, -x, -y);
+            */
+        }
+
         public static void DrawCircle(this Graphics g, Color c1, Color c2, PointF C, float r)
         {
 
